c3ecc00dec9be647ef0ec55a3c65a334790aeec9
hgdkgs <1600012764@pku.edu.cn>
2022-03-03 00:01:45 -0600

    Update aurora.py

--- src/simulator/network_simulator/pcc/aurora/aurora.py
+++ src/simulator/network_simulator/pcc/aurora/aurora.py
@@ -136,14 +136,14 @@ class ConvNet(nn.Module):
         cos_trans = torch.cos(quants * tau * 3.141592).unsqueeze(2) # (N_QUANT, N_QUANT, 1)
         rand_feat = F.relu(self.phi(cos_trans).mean(dim=1) + self.phi_bias.unsqueeze(0)).unsqueeze(0) 
         # (1, N_QUANT, 7 * 7 * 64)
-        logger.log(rand_feat.shape)
+        #logger.log(rand_feat.shape)
         x = x.view(x.size(0), -1).unsqueeze(1)  # (m, 1, 7 * 7 * 64)
-        logger.log(x)
+        #logger.log(x)
         # Zτ(x,a) ≈ f(ψ(x) @ φ(τ))a  @表示按元素相乘
         x = x * rand_feat                       # (m, N_QUANT, 7 * 7 * 64)
-        logger.log(x.shape)
+        #logger.log(x.shape)
         x = F.relu(self.fc(x))                  # (m, N_QUANT, 512)
-        logger.log(x.shape)
+        #logger.log(x.shape)
 
         # note that output of IQN is quantile values of value distribution
         action_value = self.fc_q(x).transpose(1, 2) # (m, N_ACTIONS, N_QUANT)
@@ -196,13 +196,13 @@ class DQN(object):
         # epsilon-greedy
         if np.random.uniform() >= EPSILON:
             # greedy case
-            logger.log(x)
+            #logger.log(x)
             action_value, tau = self.pred_net(x) 	# (N_ENVS, N_ACTIONS, N_QUANT)
-            logger.log(action_value)
+            #logger.log(action_value)
             action_value = action_value.mean(dim=2)
-            logger.log(action_value)
+            #logger.log(action_value)
             action = torch.argmax(action_value, dim=1).data.cpu().numpy()
-            logger.log(action)
+            #logger.log(action)
         else:
             # random exploration case
             action = np.random.randint(0, 11)
