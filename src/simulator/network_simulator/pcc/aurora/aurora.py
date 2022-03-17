82449984c232e8a42ea7d370442988da1b39d190
hgdkgs <1600012764@pku.edu.cn>
2022-03-17 16:48:27 -0500

    Update aurora.py

--- src/simulator/network_simulator/pcc/aurora/aurora.py
+++ src/simulator/network_simulator/pcc/aurora/aurora.py
@@ -139,7 +139,7 @@ class DQN():
         self.seed = random.seed(5)
         self.TAU = 1e-2
         self.GAMMA = GAMMA
-        self.UPDATE_EVERY = 50
+        self.UPDATE_EVERY = 1
         self.BATCH_SIZE = BATCH_SIZE
         self.Q_updates = 0
         self.n_step = 1
